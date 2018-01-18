32c1e485aa996bbcc997b50ca2253687308c3b33
Lindroid <linyulong2014@163.com>
2018-01-18 17:38:42 +0800

    添加了Json数据文件，并使用Gson解析。

--- app/src/main/java/com/lindroid/goodsattributeschoose/MainActivity.java
+++ app/src/main/java/com/lindroid/goodsattributeschoose/MainActivity.java
@@ -1,7 +1,14 @@
 package com.lindroid.goodsattributeschoose;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
 
 /**
  * @author Lin
@@ -12,5 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Gson gson = new Gson();
+        Reader reader = new InputStreamReader(getResources().openRawResource(R.raw.data));
+        Type type = new TypeToken<TicketBean>() {
+        }.getType();
+        TicketBean ticketBean = gson.fromJson(reader, type);
     }
 }
---
+++ app/src/main/java/com/lindroid/goodsattributeschoose/TicketBean.java
@@ -0,0 +1,111 @@
+package com.lindroid.goodsattributeschoose;
+
+import java.util.List;
+
+/**
+ * @author Lin
+ * @date 2018/1/18
+ * @function
+ */
+
+public class TicketBean {
+
+    private List<DataBean> data;
+
+    public List<DataBean> getData() {
+        return data;
+    }
+
+    public void setData(List<DataBean> data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        /**
+         * ticketID : 1
+         * price : 100
+         * date : 2月13日
+         * time : 09:00
+         * level : 硬座
+         * limitedNum : 2
+         * remainNum : 5
+         */
+
+        private int ticketID;
+        private int price;
+        private String date;
+        private String time;
+        private String level;
+        private int limitedNum;
+        private int remainNum;
+
+        public int getTicketID() {
+            return ticketID;
+        }
+
+        public void setTicketID(int ticketID) {
+            this.ticketID = ticketID;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+
+        public void setPrice(int price) {
+            this.price = price;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        public String getLevel() {
+            return level;
+        }
+
+        public void setLevel(String level) {
+            this.level = level;
+        }
+
+        public int getLimitedNum() {
+            return limitedNum;
+        }
+
+        public void setLimitedNum(int limitedNum) {
+            this.limitedNum = limitedNum;
+        }
+
+        public int getRemainNum() {
+            return remainNum;
+        }
+
+        public void setRemainNum(int remainNum) {
+            this.remainNum = remainNum;
+        }
+
+        @Override
+        public String toString() {
+            return "DataBean{" +
+                    "ticketID=" + ticketID +
+                    ", price=" + price +
+                    ", date='" + date + '\'' +
+                    ", time='" + time + '\'' +
+                    ", level='" + level + '\'' +
+                    ", limitedNum=" + limitedNum +
+                    ", remainNum=" + remainNum +
+                    '}';
+        }
+    }
+}
